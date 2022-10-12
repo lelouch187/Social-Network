a18c8b3329ed9d85bbac5b33531f0bbb61ac30ad
lelouch187 <vanushs187@gmail.com>
2022-10-12 16:57:07 +0600

    применил классовую компоненту

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -1,16 +1,19 @@
+import React from 'react';
 import ava from "../../img/ava.jpg";
 import axios from "axios";
 
 
-const FindUsers = (props) => {
 
-    if (props.users.length === 0) {
+class FindUsers extends React.Component {
+
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then (promise => props.setUsers(promise.data.users))
-    }
-
+            .then(promise => this.props.setUsers(promise.data.users))
+}
+render() {
     return <div className="col-span-8">
-        {props.users.map(u => <div
+        {this.props.users.map(u => <div
             className="border-solid border-b-2 border-slate-600 flex py-[25px] items-center"
             key={u.id}>
             <div className="px-[35px]">
@@ -19,13 +22,17 @@ const FindUsers = (props) => {
                     src={u.photos.small != null ? u.photos.small : ava}
                     alt="avatar"/>
                 {u.followed ?
-                    <button onClick={()=>{props.unfollow(u.id)}}
-                        className="inline-block w-[100px] float-right mt-[20px] hover:bg-sky-400 group rounded-md bg-sky-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
-                        type="button">Подписан
+                    <button onClick={() => {
+                        this.props.unfollow(u.id)
+                    }}
+                            className="inline-block w-[100px] float-right mt-[20px] hover:bg-sky-400 group rounded-md bg-sky-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+                            type="button">Подписан
                     </button> :
-                    <button onClick={()=>{props.follow(u.id)}}
-                        className="inline-block w-[100px] float-right mt-[20px] hover:bg-red-400 group rounded-md bg-red-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
-                        type="button">Подписаться
+                    <button onClick={() => {
+                        this.props.follow(u.id)
+                    }}
+                            className="inline-block w-[100px] float-right mt-[20px] hover:bg-red-400 group rounded-md bg-red-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+                            type="button">Подписаться
                     </button>
                 }
             </div>
@@ -35,7 +42,8 @@ const FindUsers = (props) => {
                     <p className="text-2xl mb-[20px]">{u.name}</p>
                     <p className="text-xl italic text-slate-400">{u.status}</p>
                 </div>
-                <div className="text-2xl text-right">
+                <div
+                    className="text-2xl text-right">
                     <p>{'u.location.city'},</p>
                     <p>{'u.location.country'}</p>
                 </div>
@@ -43,6 +51,7 @@ const FindUsers = (props) => {
         </div>)
         }
     </div>
+}
 }
 
 export default FindUsers
